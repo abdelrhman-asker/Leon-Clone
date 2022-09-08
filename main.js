da3ecd4e3382f834cb4b2777bc353d7661f37c6e
unknown <abdosker11@yahoo.com>
2022-09-08 02:15:37 +0200

    changes

---
+++ main.js
@@ -0,0 +1,17 @@
+function opennav() {
+    document.getElementById("secnav").style.display = "flex";
+    document.getElementById("secnavMain").style.display = "flex";
+    // document.body.style.opacity = "0.7"
+    // document.getElementById("secnav").style.opacity = "1";
+
+
+
+}
+function closenav() {
+    document.getElementById("secnav").style.display = "none"
+    document.getElementById("secnavMain").style.display = "none"
+    // document.body.style.opacity = "1"
+
+
+
+}
